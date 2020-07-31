5714dd56589f71cd77e63b279fe5ed3d4e7d1ff8
mirshafiee <b.mirshafiee90@gmail.com>
2020-07-31 19:33:21 +0430

    ViewModel Factory with the abstract factory Design Pattern

---
+++ app/src/main/java/com/bitamirshafiee/roomsample/ViewModelFactory.kt
@@ -0,0 +1,33 @@
+package com.bitamirshafiee.roomsample
+
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import com.bitamirshafiee.roomsample.data.local.DiaryRepository
+import com.bitamirshafiee.roomsample.diarylist.DiaryListViewModel
+import com.bitamirshafiee.roomsample.diarylist.EditNewDiaryViewModel
+import java.lang.IllegalArgumentException
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory(
+    private val diaryRepository: DiaryRepository,
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle? = null
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+    override fun <T : ViewModel?> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ) = with(modelClass) {
+        when {
+            isAssignableFrom(DiaryListViewModel::class.java) ->
+                DiaryListViewModel(diaryRepository)
+            isAssignableFrom(EditNewDiaryViewModel::class.java) ->
+                EditNewDiaryViewModel(diaryRepository)
+            else ->
+                throw IllegalArgumentException("UNKNOWN ViewModel class: ${modelClass.name}")
+        }
+    } as T
+}
