252c2e00839a3283491f2852630f253a7a914177
mirshafiee <b.mirshafiee90@gmail.com>
2020-07-31 19:38:13 +0430

    EditDiary Viewmodel

---
+++ app/src/main/java/com/bitamirshafiee/roomsample/diarylist/EditNewDiaryViewModel.kt
@@ -0,0 +1,10 @@
+package com.bitamirshafiee.roomsample.diarylist
+
+import androidx.lifecycle.ViewModel
+import com.bitamirshafiee.roomsample.data.local.DiaryRepository
+
+class EditNewDiaryViewModel(
+    private val diaryRepository: DiaryRepository
+) : ViewModel(){
+
+}
