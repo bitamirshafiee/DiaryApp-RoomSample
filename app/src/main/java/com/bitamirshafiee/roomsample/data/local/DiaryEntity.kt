e873cbbe66f87853efdfb7e14920faefc4f47251
mirshafiee <b.mirshafiee90@gmail.com>
2020-08-07 11:53:55 +0430

    Make DIary Entity Parcable for Bundles

--- app/src/main/java/com/bitamirshafiee/roomsample/data/local/DiaryEntity.kt
+++ app/src/main/java/com/bitamirshafiee/roomsample/data/local/DiaryEntity.kt
@@ -1,10 +1,14 @@
 package com.bitamirshafiee.roomsample.data.local
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.util.*
 
+@Parcelize
 @Entity(tableName = "diaries")
 data class DiaryEntity(
-    @PrimaryKey(autoGenerate = true) val uid : Int,
-    @ColumnInfo(name = "text") val text : String)
+    @PrimaryKey @ColumnInfo(name = "entryid") val uid : String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "text") val text : String) : Parcelable
